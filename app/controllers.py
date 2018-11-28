d8b7412d3ae2deb1e1289201c729a9dc519410db
Chad Estioco <chadestioco@gmail.com>
2018-11-29 04:55:09 +0800

    Some more layout ideas.

--- app/controllers.py
+++ app/controllers.py
@@ -5,3 +5,7 @@ bp = Blueprint("jorrvaskr", __name__)
 @bp.route("/")
 def index():
     return render_template("index.jinja")
+
+@bp.route("/session/new/", methods=("GET",))
+def session_start():
+    return render_template("session-new.jinja")
