c19f1cc6914183fc8959adeccec740ab8baa7f8a
Chad Estioco <chadestioco@gmail.com>
2018-11-30 05:13:09 +0800

    Introduce a little OO to prevent awkwardness in other pages.

--- app/static/scripts/jorrvaskr.js
+++ app/static/scripts/jorrvaskr.js
@@ -2,9 +2,14 @@ function gid(s){
     return document.getElementById(s);
 }
 
-function baseOnLoad(){
+function PageController(){
+}
+
+PageController.prototype.onLoad = function(){
     // Make the current date the default for the datepicker.
     var now = new Date();
     var sessionStartDateInput = gid("jorrvaskr-session-start-date");
     sessionStartDateInput.defaultValue = now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate();
 }
+
+var pc = new PageController();
