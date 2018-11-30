2bf8b783b829c9d49ee1c358457f809fc908be53
Chad Estioco <chadestioco@gmail.com>
2018-12-01 03:56:51 +0800

    Add start stop UI states.

--- app/static/scripts/session-new.js
+++ app/static/scripts/session-new.js
@@ -4,5 +4,16 @@ pc.onLoad = function(){
 pc.addPlayer = function(){
 }
 
-ps.startGame = function(){
+pc.startGame = function(){
+    var startPrompt = gid("jorrvaskr-start-prompt");
+    startPrompt.style.display = "none";
+    var stopPrompt = gid("jorrvaskr-stop-prompt");
+    stopPrompt.style.display = "block";
+}
+
+pc.stopGame = function(){
+    var stopPrompt = gid("jorrvaskr-stop-prompt");
+    stopPrompt.style.display = "none";
+    var startPrompt = gid("jorrvaskr-start-prompt");
+    startPrompt.style.display = "block";
 }
