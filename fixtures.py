cd9ad438d8b8b2d8e3d98da4de47b973b9f46efb
Chad Estioco <chadestioco@gmail.com>
2019-01-16 20:37:19 +0800

    Add explicit ids to fixtures.

--- fixtures.py
+++ fixtures.py
@@ -2,12 +2,12 @@ import app
 
 def insert_fixtures(session):
     from app.models import Faction, GameType
-    session.add(GameType(label="One Night"))
-    session.add(GameType(label="Ultimate"))
-    session.add(Faction(name="Werewolves"))
-    session.add(Faction(name="Villagers"))
-    session.add(Faction(name="Tanner"))
-    session.add(Faction(name="Lovers"))
+    session.add(GameType(id=1,label="One Night"))
+    session.add(GameType(id=2, label="Ultimate"))
+    session.add(Faction(id=1, name="Werewolves"))
+    session.add(Faction(id=2, name="Villagers"))
+    session.add(Faction(id=3, name="Tanner"))
+    session.add(Faction(id=4, name="Lovers"))
     session.commit()
 
 if __name__ == "__main__":
