00df14e860915f7e43c74d5fc6dc732ca4658988
Chad Estioco <chadestioco@gmail.com>
2019-03-28 02:26:20 +0800

    Finish downgrade procedure.

--- alembic/versions/bc02f88af0bb_refactor_win_logs.py
+++ alembic/versions/bc02f88af0bb_refactor_win_logs.py
@@ -7,7 +7,7 @@ Create Date: 2019-03-24 19:55:03.429092
 """
 from alembic import op
 from datetime import datetime
-from sqlalchemy.sql import distinct, select
+from sqlalchemy.sql import bindparam, distinct, select
 import sqlalchemy as sa
 
 
@@ -25,6 +25,8 @@ def upgrade():
     win_logs_table = sa.Table("win_logs", meta, autoload=True)
 
     # Get the contents of faction_win_logs from win_logs
+    # Only works because right now we are sure that win_logs has so few records.
+    # In actuality, should block this transaction.
     derived_faction_wins = conn.execute(
         select([distinct(
             win_logs_table.c.game_session_id,
@@ -90,4 +92,66 @@ def upgrade():
 
 
 def downgrade():
+    conn = op.get_bind()
+    metadata = sa.MetaData(bind=conn)
     op.rename_table("player_win_logs", "win_logs")
+
+    # win_logs will be left in an inconsistent state for the meantime.
+    op.add_column(
+        "win_logs",
+        sa.Column(
+            "faction_id",
+            sa.ForeignKey(
+                "factions.id", name="winlog_factions_fk3", ondelete="CASCADE"
+            ),
+            nullable=False
+        )
+    )
+    op.add_column(
+        "win_logs",
+        sa.Column(
+            "game_session_id",
+            sa.Integer,
+            sa.ForeignKey(
+                "game_sessions.id", name="winlog_gamesessions_fk2",
+                ondelete="CASCADE"
+            ),
+            nullable=False
+        )
+    )
+    
+    faction_wins_table = sa.Table("faction_wins", meta, autoload=True)
+    win_logs_table = sa.Table("win_logs", meta, autoload=True)
+
+    # This assumes that the tuple (game_session_id, faction_id, created_at) is a
+    # unique. While the DB does not enforce this constraint, this actually
+    # reflects the state of the data. If a bunch of super-fact werewolf-playing
+    # AI agents were to store stats in Jorrvaskr, this assumption might not hold.
+    faction_win_records = conn.execute(
+        select([
+            faction_wins_table.c.id,
+            faction_wins_table.c.game_session_id,
+            faction_wins_table.c.faction_id,
+            faction_wins_table.c.created_at
+        ]).fetchall()
+    )
+
+    for win_record in faction_win_records:
+        params = {
+            "b_faction_win_id": win_record[0],
+            "b_game_session_id": win_record[1],
+            "b_faction_id": win_record[2],
+            "b_created_at": win_record[3],
+        }
+        conn.execute(
+            win_logs_table.update()
+            .where(table.c.faction_win_log_id == bindparam("b_faction_win_id"))
+            .values(
+                faction_id=bindparam("b_faction_id"),
+                game_session_id=bindparam("b_game_session_id"),
+                created_at=bindparam("b_created_at")
+            )
+        )
+
+    op.drop_column("win_logs", "faction_win_log_id")
+    op.drop_table("faction_win_logs")
